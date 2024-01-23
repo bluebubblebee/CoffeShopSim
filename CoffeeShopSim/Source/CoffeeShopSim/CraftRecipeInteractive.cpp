827e0cd19bb265ac85594ff14fa69b64ce019907
Bee Sanz <blue.bubble.bee2@gmail.com>
2024-01-23 15:53:56 +0000

    # Updated graphics (Sushi Restaurant Kit - Quaternius): Characters, Environments and UI # Updated logic: Moved Orders, Items and Customers to DataAssets

--- CoffeeShopSim/Source/CoffeeShopSim/CraftRecipeInteractive.cpp
+++ CoffeeShopSim/Source/CoffeeShopSim/CraftRecipeInteractive.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "ShopManager.h"
 #include "CraftRecipeInteractive.h"
+#include "ShopManager.h"
+
 
 bool ACraftRecipeInteractive::TryAddIngredients(const FItem& Item)
 {	
