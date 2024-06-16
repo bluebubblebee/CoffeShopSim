7ce6e63eaf029ac50a4104b33b4ab76ba8f6c2e3
Bee Sanz <blue.bubble.bee2@gmail.com>
2024-06-16 10:47:57 +0100

    Update to 5.4

--- CoffeeShopSim/Source/CoffeeShopSimEditor.Target.cs
+++ CoffeeShopSim/Source/CoffeeShopSimEditor.Target.cs
@@ -8,8 +8,9 @@ public class CoffeeShopSimEditorTarget : TargetRules
 	public CoffeeShopSimEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		bOverrideBuildEnvironment = true;
+        DefaultBuildSettings = BuildSettingsVersion.V4;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+        //bOverrideBuildEnvironment = true;
         ExtraModuleNames.Add("CoffeeShopSim");
 	}
 }
